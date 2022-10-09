8a2667562b6ca8405c85404b8961c53f49a68c0f
BaiMeow <baishu2003@outlook.com>
2022-10-09 15:00:08 +0800

    refactor: using wg

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,21 +1,33 @@
 export default {
-  title: 'DN11',
-  description: 'An intranet of Building 11',
-  outDir: '../dist',
-  themeConfig: {
-    sidebar: [
-      {
-        text: 'Start',
-        items: [
-          { text: 'GettingStart', link: '/start' },
+    title: 'DN11',
+    description: 'An intranet of Building 11',
+    outDir: '../dist',
+    themeConfig: {
+        sidebar: [{
+                text: '简介',
+                items: [
+                    { text: '开始', link: '/start' },
+                ]
+            },
+            {
+                text: '接入',
+                items: [
+                    { text: '子网并入', link: '/mergeSubnet' },
+                    { text: '单设备接入', link: '/singleDevice' }
+                ]
+            },
+            {
+                text: '节点',
+                items: [
+                    { text: '列表', link: '/peerList' }
+                ]
+            },
+            {
+                text: '服务',
+                items: [
+                    { text: 'Samba', link: '/service/samba' }
+                ]
+            }
         ]
-      },
-      {
-        text: 'Guide',
-        items: [
-          { text: 'Use Tailscale', link: '/tailscale' },
-        ]
-      }
-    ]
-  }
+    }
 }
---
+++ docs/service/samba.md
@@ -0,0 +1,12 @@
+# Samba
+
+这里记录了一些公开的Samba服务
+
+## BaiMeow Share
+
+对外分享一下文件
+
+- `\\172.16.4.5\share`
+- 只读
+
+维护: BaiMeow
