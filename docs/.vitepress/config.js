0793c37c0b4fc2bacb111dae9421d134dfbf612a
camera-2018 <2907618001@qq.com>
2023-04-17 19:46:50 +0800

    feat: 增加搜索、页脚

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,40 +1,47 @@
 export default {
-    title: 'DN11',
-    description: 'An intranet of Building 11',
-    outDir: '../dist',
-    themeConfig: {
-        sidebar: [{
-                text: '简介',
-                items: [
-                    { text: '开始', link: '/introduce/start' },
-                ]
-            },
-            {
-                text: '接入',
-                items: [
-                    { text: '子网并入', link: '/connect/subnet' },
-                    { text: '单设备接入', link: '/connect/single' },
-                    { text: 'OSPF', link: '/connect/ospf' },
-                ]
-            },
-            {
-                text: '节点',
-                items: [
-                    { text: '列表', link: '/peer/list' }
-                ]
-            },
-            {
-                text: '服务',
-                items: [
-                    { text: 'Samba', link: '/service/samba' }
-                ]
-            },
-            {
-                text: '其他',
-                items: [
-                    { text: 'DDNS', link: '/mics/ddns' }
-                ]
-            }
-        ]
+  title: 'DN11',
+  description: 'An intranet of Building 11',
+  outDir: '../dist',
+  themeConfig: {
+    sidebar: [{
+      text: '简介',
+      items: [
+        { text: '开始', link: '/introduce/start' },
+      ]
+    },
+    {
+      text: '接入',
+      items: [
+        { text: '子网并入', link: '/connect/subnet' },
+        { text: '单设备接入', link: '/connect/single' },
+        { text: 'OSPF', link: '/connect/ospf' },
+      ]
+    },
+    {
+      text: '节点',
+      items: [
+        { text: '列表', link: '/peer/list' }
+      ]
+    },
+    {
+      text: '服务',
+      items: [
+        { text: 'Samba', link: '/service/samba' }
+      ]
+    },
+    {
+      text: '其他',
+      items: [
+        { text: 'DDNS', link: '/mics/ddns' }
+      ]
     }
+    ],
+    search: {
+      provider: 'local'
+    },
+    footer: {
+      message: 'Made with ❤️ by DN11 team',
+      copyright: 'Copyright © 2019-present Evan You && DN11 team'
+    }
+  }
 }
