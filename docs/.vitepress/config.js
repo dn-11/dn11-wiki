71247ac1a9098bfab4aa62489e9b8d042f55cbb5
camera-2018 <2907618001@qq.com>
2024-09-15 00:49:09 +0800

    feat: add favicon and mermaid support

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,9 +1,22 @@
 import PanguPlugin from 'markdown-it-pangu'
-import { birdConf } from './theme/shikiji';
-export default {
+import { birdConf } from './theme/shikiji'
+import { withMermaid } from "vitepress-plugin-mermaid"
+export default withMermaid({
   title: 'DN11',
   description: 'An intranet of Building 11',
   outDir: '../dist',
+  mermaid:{
+    //mermaidConfig !theme here works for ligth mode since dark theme is forced in dark mode
+  },
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ],
   markdown: {
     lineNumbers: true,
     config: (md) => {
@@ -85,4 +98,4 @@ export default {
       copyright: 'Copyright © DN11 team'
     }
   }
-}
+})
